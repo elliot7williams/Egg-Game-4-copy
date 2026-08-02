add2e498929f0818f459266e0c0bdd3573dcf5fb
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:10:10 -0400

    Add Egg Game 4 copy/Egg Game 4Tests/Egg_Game_4Tests.swift

---
+++ Egg Game 4 copy/Egg Game 4Tests/Egg_Game_4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Egg_Game_4Tests.swift
+//  Egg Game 4Tests
+//
+//  Created by Elliot Williams on 2025-07-02.
+//
+
+import Testing
+@testable import Egg_Game_4
+
+struct Egg_Game_4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
